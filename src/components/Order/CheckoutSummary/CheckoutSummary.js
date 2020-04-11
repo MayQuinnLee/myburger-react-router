e8bfd1858997a0ba8e19d712f23a42777d3d426f
Quinn94 <mayquinn94@gmail.com>
2020-04-11 12:12:53 +0800

    routing

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.module.css';
+
+const checkoutSummary = (props) => {
+    return(
+        <div className={classes.CheckoutSummary}>
+            <h1> We hope you'll like the burger!</h1>
+
+            <div style={{margin: 'auto', width: '100%'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+
+            <Button 
+            btnType='Danger' 
+            clicked={props.checkoutCancelled} >CANCEL</Button>
+            <Button 
+            btnType='Success' 
+            clicked={props.checkoutContinue} >CONTINUE</Button>
+
+        </div>
+    )
+}
+
+export default checkoutSummary;
