f8153b85056ccb4e10d9c5a22889875c78c454d3
Quinn94 <mayquinn94@gmail.com>
2020-04-12 21:38:56 +0800

    order page

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
+class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res=>{
+                //firebase will return object and we convert to array
+                const fetchedOrders =[];
+                for(let key in res.data){
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key,
+                    });
+                }
+                console.log(fetchedOrders);
+                this.setState({loading: false})
+            })
+            .catch(err=>{
+                this.setState({loading: false})
+            })
+    }
+
+    render() {
+
+        return(
+            <div>
+                <Order />
+                <Order />
+            </div>
+        )
+    }
+}
+
+export default withErrorHandler(Orders,axios);
